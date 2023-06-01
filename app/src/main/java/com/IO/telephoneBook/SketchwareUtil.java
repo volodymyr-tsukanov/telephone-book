191c0c0ec0a432d67ed8ee863b25a53031dfc090
volodymyr-tsukanov <134626794+volodymyr-tsukanov@users.noreply.github.com>
2023-06-01 13:17:27 +0200

    Update SketchwareUtil.java

--- app/src/main/java/com/IO/telephoneBook/SketchwareUtil.java
+++ app/src/main/java/com/IO/telephoneBook/SketchwareUtil.java
@@ -11,7 +11,8 @@ import android.widget.*;
 import java.io.*;
 import java.util.*;
 
-public class SketchwareUtil {
+
+public class CustomUtil {
 
 	public static int TOP = 1;
 	public static int CENTER = 2;
@@ -81,52 +82,6 @@ public class SketchwareUtil {
 		_toast.show();
 	}
 
-	public static void sortListMap(final ArrayList<HashMap<String, Object>> listMap, final String key, final boolean isNumber, final boolean ascending) {
-		Collections.sort(listMap, new Comparator<HashMap<String, Object>>() {
-			public int compare(HashMap<String, Object> _compareMap1, HashMap<String, Object> _compareMap2) {
-				if (isNumber) {
-					int _count1 = Integer.valueOf(_compareMap1.get(key).toString());
-					int _count2 = Integer.valueOf(_compareMap2.get(key).toString());
-					if (ascending) {
-						return _count1 < _count2 ? -1 : _count1 < _count2 ? 1 : 0;
-					} else {
-						return _count1 > _count2 ? -1 : _count1 > _count2 ? 1 : 0;
-					}
-				} else {
-					if (ascending) {
-						return (_compareMap1.get(key).toString()).compareTo(_compareMap2.get(key).toString());
-					} else {
-						return (_compareMap2.get(key).toString()).compareTo(_compareMap1.get(key).toString());
-					}
-				}
-			}
-		});
-	}
-
-	public static void CropImage(Activity _activity, String _path, int _requestCode) {
-		try {
-			Intent _intent = new Intent("com.android.camera.action.CROP");
-			File _file = new File(_path);
-			Uri _contentUri = Uri.fromFile(_file);
-			_intent.setDataAndType(_contentUri, "image/*");
-			_intent.putExtra("crop", "true");
-			_intent.putExtra("aspectX", 1);
-			_intent.putExtra("aspectY", 1);
-			_intent.putExtra("outputX", 280);
-			_intent.putExtra("outputY", 280);
-			_intent.putExtra("return-data", false);
-			_activity.startActivityForResult(_intent, _requestCode);
-		} catch (ActivityNotFoundException _e) {
-			Toast.makeText(_activity, "Your device doesn't support the crop action!", Toast.LENGTH_SHORT).show();
-		}
-	}
-
-	public static boolean isConnected(Context _context) {
-		ConnectivityManager _connectivityManager = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo _activeNetworkInfo = _connectivityManager.getActiveNetworkInfo();
-		return _activeNetworkInfo != null && _activeNetworkInfo.isConnected();
-	}
-
 	public static String copyFromInputStream(InputStream _inputStream) {
 		ByteArrayOutputStream _outputStream = new ByteArrayOutputStream();
 		byte[] _buf = new byte[1024];
@@ -174,16 +129,6 @@ public class SketchwareUtil {
 		return random.nextInt(_max - _min + 1) + _min;
 	}
 
-	public static ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
-		ArrayList<Double> _result = new ArrayList<Double>();
-		SparseBooleanArray _arr = _list.getCheckedItemPositions();
-		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
-			if (_arr.valueAt(_iIdx))
-				_result.add((double) _arr.keyAt(_iIdx));
-		}
-		return _result;
-	}
-
 	public static float getDip(Context _context, int _input) {
 		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, _context.getResources().getDisplayMetrics());
 	}
@@ -194,16 +139,5 @@ public class SketchwareUtil {
 
 	public static int getDisplayHeightPixels(Context _context) {
 		return _context.getResources().getDisplayMetrics().heightPixels;
-	}
-
-	public static void getAllKeysFromMap(Map<String, Object> _map, ArrayList<String> _output) {
-		if (_output == null) return;
-		_output.clear();
-		if (_map == null || _map.size() < 1) return;
-		Iterator _itr = _map.entrySet().iterator();
-		while (_itr.hasNext()) {
-			Map.Entry<String, String> _entry = (Map.Entry) _itr.next();
-			_output.add(_entry.getKey());
-		}
 	}
 }
